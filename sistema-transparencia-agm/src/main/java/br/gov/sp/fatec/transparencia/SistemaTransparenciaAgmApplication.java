43e1c267674ee1ad580c6f9e3218bf52715e6816
biel-fredy <biel1801fredy@gmail.com>
2020-05-13 08:06:13 -0300

    project: criacao do projeto inicial apenas com as configurações padroes do spring

---
+++ sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/SistemaTransparenciaAgmApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.transparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTransparenciaAgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTransparenciaAgmApplication.class, args);
+	}
+
+}
---
+++ sistema-transparencia-agm/src/test/java/br/gov/sp/fatec/transparencia/SistemaTransparenciaAgmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.transparencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransparenciaAgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
