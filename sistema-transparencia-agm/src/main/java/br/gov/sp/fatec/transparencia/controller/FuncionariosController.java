5235b6cc8ee941bd1b2e95b9e72c57c38983f726
biel-fredy <biel1801fredy@gmail.com>
2020-05-14 11:11:47 -0300

    feat: Pesquisa por filtro, dinamica e pelos campos nome e cargo

--- sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/controller/FuncionariosController.java
+++ sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/controller/FuncionariosController.java
@@ -3,6 +3,8 @@ package br.gov.sp.fatec.transparencia.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.gov.sp.fatec.transparencia.repository.Funcionarios;
@@ -20,5 +22,13 @@ public class FuncionariosController {
 		modelAndView.addObject("funcionarios",funcionarios.findAll());
 		return modelAndView;
 	}
-
+	
+	@PostMapping("**/pesquisarfuncionario")
+	public ModelAndView listaFuncionariosComFiltro(@RequestParam("nomepesquisa") String nome, @RequestParam("cargopesquisa") String cargo, @RequestParam("salpesquisa") String salario) {
+		ModelAndView modelAndView = new ModelAndView("ListaFuncionarios");
+		nome = "%" + nome.toUpperCase() + "%";
+		cargo = "%" + cargo.toUpperCase() + "%";
+		modelAndView.addObject("funcionarios", funcionarios.findFuncionarioByName(nome, cargo));
+		return modelAndView;
+	}
 }
--- sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/repository/Funcionarios.java
+++ sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/repository/Funcionarios.java
@@ -2,13 +2,18 @@ package br.gov.sp.fatec.transparencia.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.gov.sp.fatec.transparencia.model.Funcionario;
 
+@Repository
+@Transactional
 public interface Funcionarios extends JpaRepository<Funcionario, Long> {
 	
-	@Query("selet f from funcionario f where f.fnc_nome like %?1%")
-	public List<Funcionario> findFuncionarioByName(String nome);
+	@Query("select f from Funcionario f where f.fnc_nome like ?1 and f.fnc_cargo like ?2")
+	List<Funcionario> findFuncionarioByName(String nome, String cargo);
 }
