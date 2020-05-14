7af30eec9097366e94ad30b9d4669bdeb39fdc8d
biel-fredy <biel1801fredy@gmail.com>
2020-05-14 09:24:16 -0300

    feat: Alteracao no repository Funcionarios e criação do form de busca no ListarFuncionarios.html

--- sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/model/Funcionario.java
+++ sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/model/Funcionario.java
@@ -6,13 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 public class Funcionario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long fnc_id;
 	
 	private String fnc_nome;
--- sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/repository/Funcionarios.java
+++ sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/repository/Funcionarios.java
@@ -1,9 +1,14 @@
 package br.gov.sp.fatec.transparencia.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.gov.sp.fatec.transparencia.model.Funcionario;
 
 public interface Funcionarios extends JpaRepository<Funcionario, Long> {
-
+	
+	@Query("selet f from funcionario f where f.fnc_nome like %?1%")
+	public List<Funcionario> findFuncionarioByName(String nome);
 }
