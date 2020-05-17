1dfe9dfaf21fbcb33ae024c3f646cc93f9bca5e8
biel-fredy <biel1801fredy@gmail.com>
2020-05-17 11:36:32 -0300

    feat: adicionado tela inicial, icones

--- sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/controller/FuncionariosController.java
+++ sistema-transparencia-agm/src/main/java/br/gov/sp/fatec/transparencia/controller/FuncionariosController.java
@@ -1,9 +1,6 @@
 package br.gov.sp.fatec.transparencia.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
